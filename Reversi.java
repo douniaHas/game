5d376448c7531a87432e3f53443b3f4d33c8e138
Dounia Hassani <hassani.dounia@gmail.com>
2019-02-10 15:22:00 +0100

    refactoring methode transformer liste en grille

--- Reversi.java
+++ Reversi.java
@@ -1,12 +1,14 @@
 package fr.erdf.siae.business.a0x;
 
 public class Reversi {
+    public static final int S_DIM = 8;
+    public static final char S_RETOUR_A_LA_LIGNE = '\n';
+
     public static char[][] transformerListeEnGrille(String grilleLineaire) {
-        int dim = 8;
-        char[][] grille = new char[dim][dim];
+        char[][] grille = new char[S_DIM][S_DIM];
         for (int i = 0; i < grilleLineaire.length() - 1; i++) {
-            if (grilleLineaire.charAt(i) != '\n') {
-                grille[i / (dim + 1)][i % (dim + 1)] = grilleLineaire.charAt(i);
+            if (grilleLineaire.charAt(i) != S_RETOUR_A_LA_LIGNE) {
+                grille[i / (S_DIM + 1)][i % (S_DIM + 1)] = grilleLineaire.charAt(i);
             }
         }
         return grille;
